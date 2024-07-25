119e7125102f33d25d38630056f70e3910c5f945
rupigithub <singhrupika4@gmail.com>
2024-07-26 01:37:10 +0530

    commit again

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,9 @@ import '../styles/App.css';
 
 function App(){
     return(
-        <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+        <div id="main">
+            <p>Now I can render any React component on any DOM node I want using ReactDOM.render</p>
+        </div>
     )
 }
 
